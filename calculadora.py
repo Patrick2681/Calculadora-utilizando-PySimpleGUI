092beebdad68aaeff3b7c185a944c13b9b47ecc2
Patrick Ferreira <55933442+Patrick2681@users.noreply.github.com>
2020-11-11 01:24:52 -0300

    Atualização de aparencia

--- calculadora.py
+++ calculadora.py
@@ -6,17 +6,18 @@ class Calculadora:
         self.calculo = ''
         self.numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         layout = [
-            [sg.Text('', size=(23, 2), justification='right', relief=sg.RELIEF_RIDGE, background_color='#F7F3EC', text_color='#000000', key='display')],
+            [sg.Text(size=(20, 2), justification='right', relief=sg.RELIEF_RIDGE, background_color='#F7F3EC', text_color='#000000', key='display',border_width=10)],
             [sg.HorizontalSeparator()],
-            [sg.Button('C', size=(23, 2))],
+            [sg.Button('C', size=(22, 2))],
             [sg.Button('7', size=(4, 2)), sg.Button('8', size=(4, 2)), sg.Button('9', size=(4, 2)), sg.Button('+', size=(4, 2))],
             [sg.Button('4', size=(4, 2)), sg.Button('5', size=(4, 2)), sg.Button('6', size=(4, 2)), sg.Button('-', size=(4, 2))],
             [sg.Button('1', size=(4, 2)), sg.Button('2', size=(4, 2)), sg.Button('3', size=(4, 2)), sg.Button('*', size=(4, 2))],
             [sg.Button('0', size=(10, 2)), sg.Button('.', size=(4, 2)), sg.Button('/', size=(4, 2))],
-            [sg.Button('=', size=(23, 2))]
+            [sg.Button('=', size=(22, 2))]
         ]
         # sg.theme('LightBrown1')
         sg.theme('BlueMono')
+        sg.set_options(element_padding=[4,4])
         self.janela = sg.Window('Calculadora', layout)
 
     def iniciar(self):
